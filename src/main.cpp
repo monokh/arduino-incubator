6c7c77fae18e484f3fcd97bbc981be582dac821f
mo <mnokhb@gmail.com>
2017-05-13 22:39:18 +0100

    Temp sensor

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,37 @@
 #include <Arduino.h>
+#include <DHT.h>
 
-int LED_PIN = 5;
+#define DHTTYPE DHT22
+#define DHTPIN 2
+#define LEDPIN 13
+
+DHT dht(DHTPIN, DHTTYPE);
 
 void setup()
 {
   Serial.begin(9600);
   Serial.println("Starting up");
-  pinMode(LED_PIN, OUTPUT);
+  pinMode(LEDPIN, OUTPUT);
+  digitalWrite(LEDPIN, LOW);
+  dht.begin();
 }
 
 void loop()
 {
-  delay(100);
-  analogWrite(LED_PIN, 255);
+  delay(1000);
+  float temperature = dht.readTemperature();
+  float humidity = dht.readHumidity();
+  
+  Serial.print("Temp: ");
+  Serial.print(temperature);
+  Serial.print(" *C\t");
+  Serial.print("Humidity: ");
+  Serial.print(humidity);
+  Serial.println(" %");
+
+  if (temperature > 37.0f) {
+    digitalWrite(LEDPIN, HIGH);
+  } else {
+    digitalWrite(LEDPIN, LOW);
+  }
 }
