63d22cc55c78323b3221bb9f477c166c0b5b9e62
monokh <mnokhb@gmail.com>
2022-03-01 23:47:29 +0000

    final

--- src/main.cpp
+++ src/main.cpp
@@ -4,34 +4,56 @@
 #include <EEPROM.h>
 #include <IRremote.h>
 #include <Timer.h>
+#include <EasyBuzzer.h>
 #include <math.h>
 
-#define IR_PIN A4
+// Control layout
+// 69  70  71
+// 68  64  67
+// 7   21  9
+// 22  25  13
+// 0   24  94
+// 8   28  90
+// 66  82  74
+#define IR_PIN 8
 #define IR_READ_INTERVAL 500
 #define IR_BLANK_CODE 4294967295 // ???
 #define IR_TOGGLE_MODE_COMMAND 70
+#define IR_TOGGLE_TURN_OFF_ALARM_COMMAND 71
+#define IR_TOGGLE_LIGHT_COMMAND 69
 
 #define DHT_TYPE DHT22
-#define DHT_PIN A1
+#define DHT_PIN 9
 
+// Top Row:    1 A F  2 3 B
+// Bottom Row: E D DP C G 4
 #define DISPLAY_NUM_DIGITS 4
 #define DISPLAY_DIGIT_PINS \
   {                        \
-    2, 3, 4, 5             \
+    2, 5, 6, A5            \
   }
-#define DISPLAY_SEGMENTS_PINS  \
-  {                            \
-    6, 7, 8, 9, 10, 11, 12, 13 \
+#define DISPLAY_SEGMENTS_PINS   \
+  {                             \
+    3, 7, A3, A1, A0, 4, A4, A2 \
   }
 #define DISPLAY_COMMON_TYPE COMMON_CATHODE
 #define DISPLAY_BRIGHTNESS 10
 #define DISPLAY_MODE_TEMPERATURE 1
 
-#define HEAT_SWITCH_PIN A2
-#define LIGHT_SWITCH_PIN A3
-#define MINIMUM_TEMPERATURE 37.5f
-#define MAXIMUM_TEMPERATURE 37.5f
+#define LIGHT_SWITCH_PIN 10
+#define HEAT_SWITCH_PIN 11
+#define MINIMUM_TEMPERATURE 36.9f
+#define MAXIMUM_TEMPERATURE 37.0f
 #define CHECK_INTERVAL 10000
+
+#define ALARM_PIN 12
+#define ALARM_FREQUENCY 1000
+#define ALARM_DURATION 100
+#define ALARM_COOLDOWN 3600000
+#define HIGH_TEMPERATURE_ALARM 38.0f
+#define LOW_TEMPERATURE_ALARM 36.0f
+#define HIGH_HUMIDITY_ALARM 80
+#define LOW_HUMIDITY_ALARM 58
 
 enum DisplayMode
 {
@@ -39,15 +61,40 @@ enum DisplayMode
   Humidity
 };
 
-SevSeg sevseg; //Instantiate a seven segment object
+SevSeg sevseg; // Instantiate a seven segment object
 Timer t;
+Timer alarmCooldownTimer;
 
 DHT dht(DHT_PIN, DHT_TYPE);
 float temperature = 0.0f;
 float humidity = 0.0f;
-bool lightOn = false;
-bool lightEnabled = true;
 bool displayMode = DisplayMode::Temperature;
+
+bool tempDisplayEnabled = false;
+char *tempDisplayString;
+
+uint8_t alarm;
+bool alarmOn = false;
+bool alarmCooldown = false;
+
+bool lightOn = true;
+
+void tempDisplayOff()
+{
+  tempDisplayEnabled = false;
+}
+
+void tempDisplay(char *string)
+{
+  tempDisplayEnabled = true;
+  tempDisplayString = string;
+  t.after(5000, tempDisplayOff);
+}
+
+void setupBuzzer()
+{
+  EasyBuzzer.setPin(ALARM_PIN);
+}
 
 void setupIR()
 {
@@ -57,10 +104,9 @@ void setupIR()
 void setupHeater()
 {
   pinMode(HEAT_SWITCH_PIN, OUTPUT);
-  digitalWrite(HEAT_SWITCH_PIN, LOW);
+  digitalWrite(HEAT_SWITCH_PIN, HIGH);
   pinMode(LIGHT_SWITCH_PIN, OUTPUT);
-  digitalWrite(LIGHT_SWITCH_PIN, HIGH);
-  lightOn = false;
+  digitalWrite(LIGHT_SWITCH_PIN, LOW);
 }
 
 void setupSensor()
@@ -76,17 +122,33 @@ void setupDisplay()
   sevseg.setBrightness(DISPLAY_BRIGHTNESS);
 }
 
-void writeDisplay() {
+void displayTemperatureHumidity()
+{
   char buf[5];
   if (displayMode == DisplayMode::Temperature)
   {
     String(temperature, 2).toCharArray(buf, 10);
     buf[4] = 'C';
-  } else if (displayMode == DisplayMode::Humidity) {
+  }
+  else if (displayMode == DisplayMode::Humidity)
+  {
     String(humidity, 2).toCharArray(buf, 10);
     buf[4] = 'P';
   }
   sevseg.setChars(buf);
+}
+
+void writeDisplay()
+{
+  if (tempDisplayEnabled)
+  {
+    sevseg.setChars(tempDisplayString);
+  }
+  else
+  {
+    displayTemperatureHumidity();
+  }
+
   sevseg.refreshDisplay();
 }
 
@@ -96,32 +158,61 @@ void readSensor()
   humidity = dht.readHumidity();
 }
 
+void turnOnLight()
+{
+  digitalWrite(LIGHT_SWITCH_PIN, LOW);
+}
+
 void turnOffLight()
 {
-  lightOn = false;
   digitalWrite(LIGHT_SWITCH_PIN, HIGH);
 }
 
-void turnOnLight()
+void turnOnHeater()
 {
-  lightOn = true;
-  digitalWrite(LIGHT_SWITCH_PIN, LOW);
+  digitalWrite(HEAT_SWITCH_PIN, LOW);
+}
+
+void turnOffHeater()
+{
+  digitalWrite(HEAT_SWITCH_PIN, HIGH);
 }
 
 void handleHeat()
 {
   if (temperature <= MINIMUM_TEMPERATURE)
   {
-    digitalWrite(HEAT_SWITCH_PIN, HIGH);
-    if (lightEnabled)
-    {
-      turnOnLight();
-    }
+    turnOnHeater();
   }
   else if (temperature > MAXIMUM_TEMPERATURE)
   {
-    digitalWrite(HEAT_SWITCH_PIN, LOW);
-    turnOffLight();
+    turnOffHeater();
+  }
+}
+
+void triggerAlarm()
+{
+  if (!alarmOn && !alarmCooldown)
+  {
+    alarmOn = true;
+    alarm = t.every(5000, []()
+                    { EasyBuzzer.beep(ALARM_FREQUENCY, 1); });
+  }
+}
+
+void handleAlarm()
+{
+  if (temperature < LOW_TEMPERATURE_ALARM || temperature > HIGH_TEMPERATURE_ALARM)
+  {
+    triggerAlarm();
+  }
+  else if (humidity < LOW_HUMIDITY_ALARM || humidity > HIGH_HUMIDITY_ALARM)
+  {
+    triggerAlarm();
+  }
+  else if (alarmOn)
+  {
+    t.stop(alarm);
   }
 }
 
@@ -133,10 +224,32 @@ void handleCommand(unsigned long command)
   if (command == IR_TOGGLE_MODE_COMMAND)
   {
     displayMode = displayMode == DisplayMode::Temperature ? DisplayMode::Humidity : DisplayMode::Temperature;
-  } else {
-    Serial.print("Received unknown command: ");
-    Serial.print(command);
-    Serial.println();
+  }
+  else if (command == IR_TOGGLE_TURN_OFF_ALARM_COMMAND)
+  {
+    alarmCooldown = true;
+    alarmOn = false;
+    t.stop(alarm);
+    alarmCooldownTimer.after(ALARM_COOLDOWN, []()
+                             { alarmCooldown = false; });
+  }
+  else if (command == IR_TOGGLE_LIGHT_COMMAND)
+  {
+    lightOn = !lightOn;
+    if (lightOn)
+    {
+      turnOnLight();
+    }
+    else
+    {
+      turnOffLight();
+    }
+  }
+  else
+  {
+    // Serial.print("Received unknown command: ");
+    // Serial.print(command);
+    // Serial.println();
   }
 }
 
@@ -153,17 +266,14 @@ void check()
 {
   readSensor();
   handleHeat();
-}
-
-void setupMemory() // RUN ONLY ONCE THE FIRST TIME CODE IS DEPLOYED
-{
+  handleAlarm();
 }
 
 void setup()
 {
-  Serial.begin(9600);
-  Serial.println("Starting up");
-  // setupMemory(); RUN ONLY ONCE THE FIRST TIME CODE IS DEPLOYED
+  // Serial.begin(9600);
+  // Serial.println("Starting up");
+  setupBuzzer();
   setupSensor();
   setupHeater();
   setupIR();
@@ -175,5 +285,7 @@ void setup()
 void loop()
 {
   writeDisplay();
+  EasyBuzzer.update();
   t.update();
+  alarmCooldownTimer.update();
 }
